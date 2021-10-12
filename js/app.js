a6f2f2f62fee8669c4dc0964b5fda7ba884e43b3
al-raiyan <alraiyan5@gmail.com>
2021-10-12 23:42:47 +0600

    4&5 requarments

--- js/app.js
+++ js/app.js
@@ -237,6 +237,7 @@ const addToCart = (id, price) => {
 
   updateTaxAndCharge();
   document.getElementById("total-Products").innerText = count;
+  updateTotal();
 };
 
 const getInputValue = (id) => {
@@ -250,12 +251,12 @@ const updatePrice = (id, value) => {
   const convertedOldPrice = getInputValue(id);
   const convertPrice = parseFloat(value);
   const total = convertedOldPrice + convertPrice;
-  document.getElementById(id).innerText = Math.round(total);
+  document.getElementById(id).innerText = Math.round(total * 100) / 100.0;
 };
 
 // set innerText function
 const setInnerText = (id, value) => {
-  document.getElementById(id).innerText = Math.round(value);
+  document.getElementById(id).innerText = Math.round(value * 100) / 100.0;
 };
 
 // update delivery charge and total Tax
